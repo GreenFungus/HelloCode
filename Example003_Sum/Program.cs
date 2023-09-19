be631e59510160e9d8eda54af0e1b9c4f8532b20
ThousandDevils <c32flag@gmail.com>
2023-09-19 22:16:26 +0300

    add example 003 sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string a, b;
+Console.WriteLine("Enter 1st number:");
+a = Console.ReadLine();
+Console.WriteLine("Enter 2nd number:");
+b = Console.ReadLine();
+int sum = (Convert.ToInt16(a) + Convert.ToInt16(b));
+Console.Write("Sum = ");
+Console.Write(sum);
